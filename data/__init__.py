0732065312c4956aeb4131cba12212ca6131e274
Haiyu Wu <hwu6@daccssfe.crc.nd.edu>
2025-05-15 09:31:10 -0400

    fix the incompatible issue

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,4 @@
 from .data_loader_train_lmdb import LMDBDataLoader
-from .data_loader_train_webdataset import WebDataLoader
 from .load_test_sets_recognition import get_val_pair
 from .dist import setup_seed, worker_init_fn
 from .feature_extraction_loader import TestDataLoader
