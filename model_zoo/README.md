944667e1c39947c74080a512f62a089ccb6be171
Haiyu Wu <hwu6@daccssfe.crc.nd.edu>
2026-02-28 15:17:32 -0500

    deterministic test and updated testing results

--- model_zoo/README.md
+++ model_zoo/README.md
@@ -2,36 +2,35 @@
 Models are trained with ResNet100 backbone and three (**MS1MV2, WebFace4M, and Glint360K**) training sets.
 
 **All the models here are trained with RBG images and ```mean=0.5, std=0.5``` image normalization, which is different from the setting of the original AdaFace and MagFace.
-Also, the accuracies are calculated with the merged features of the original and horizontally flipped images, where the original MagFace does not do it. These could cause the difference of the paper-reported accuracy and the accuracy in these tables.** 
+Also, the accuracies are calculated with the merged features of the original and horizontally flipped images, where the original MagFace does not do it. These could cause the difference of the paper-reported accuracy and the accuracy in these tables.**
 
-| MS1MV2                         |  Dataset  |  LFW  | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse |                                             Weights                                              |
-|--------------------------------|:---------:|:-----:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:------------------------------------------------------------------------------------------------:|
-| CosFace-R100 (m=0.35)[6]       |  MS1MV2   | 99.75 | 98.41  | 93.25 |  98.33   | 95.93 |    -    |    -    | [Gdrive](https://drive.google.com/file/d/1FzsD117ESm7RDXE2DMvoQmAdkL7W2sa0/view?usp=drive_link)  |
-| SphereFace-R100 (m=1.7)[7]     |  MS1MV2   | 99.83 | 98.40  | 92.67 |  98.23   | 96.07 |    -    |    -    | [Gdrive](https://drive.google.com/file/d/1vCMSDF65bslXcU0kxCadBrdW48v-7oPt/view?usp=drive_link)  |
-| ArcFace-R100 (m=0.5)[1]        |  MS1MV2   | 99.80 | 98.49  | 93.35 |  98.00   | 96.05 |  91.45  |  81.27  | [Gdrive](https://drive.google.com/file/d/1MH2eCU_II2nUtkDHgyD0Scxua1MDZtdE/view?usp=drive_link)  |
-| ArcFace-R100 (combined)[1]     |  MS1MV2   | 99.80 | 98.50  | 93.17 |  98.07   | 96.18 |    -    |    -    | [Gdrive](https://drive.google.com/file/d/1Uznh1O0EJoD34A3YchvI7FQ1G67rNGKP/view?usp=drive_link)  |
-| CurricularFace-R100 (m=0.5)[2] |  MS1MV2   | 99.78 | 98.44  | 92.95 |  98.05   | 96.08 |  91.13  |  81.58  | [Gdrive](https://drive.google.com/file/d/1OPTjbvgBnVBVrttJKvOzevG9sNQXjA1j/view?usp=drive_link)  |
-| MagFace-R100[3]                |  MS1MV2   | 99.82 | 98.21  | 92.67 |  98.15   | 96.13 |  92.65  |  82.37  | [Gdrive](https://drive.google.com/file/d/1h_V93Sc1NB5eLW26-pB7KCB7-BSCYHZj/view?usp=drive_link)  |
-| AdaFace-R100 (m=0.4)[4]        |  MS1MV2   | 99.82 | 98.63  | 93.05 |  98.20   | 96.15 |  92.05  |  82.03  | [Gdrive](https://drive.google.com/file/d/1a0BkAUwFC8O_sR2cW0NOM93zgOKBWbsr/view?usp=drive_link)  |
-| UniFace-R100 (m=0.5)[5]        |  MS1MV2   | 99.78 | 98.49  | 93.28 |  98.02   | 96.10 |  91.38  |  82.08  | [Gdrive](https://drive.google.com/file/d/1TgO7RgXPoMoM6ESIj7h09WYhotou47vD/view?usp=drive_link)  |
+| Model                         |  Dataset  | LFW | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse | Twins | Weights |
+|:------------------------------|:---------:|:------:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:------:|:------------------------------------------------------------------------------------------------:
+| AdaFace-R100 (m=0.4)       | MS1MV2    | 99.82 | 98.63 | 93.05 | 98.13 | 96.15 | 92.35 | 82.32 | 68.57 | [Gdrive](https://drive.google.com/file/d/1a0BkAUwFC8O_sR2cW0NOM93zgOKBWbsr/view?usp=drive_link) |
+| ArcFace-R100 (m=0.5)       | MS1MV2    | 99.80 | 98.49 | 93.35 | 98.00 | 96.05 | 91.72 | 81.80 | 68.28 | [Gdrive](https://drive.google.com/file/d/1MH2eCU_II2nUtkDHgyD0Scxua1MDZtdE/view?usp=drive_link) |
+| ArcFace-R100 (combined)    | MS1MV2    | 99.80 | 98.53 | 93.15 | 98.07 | 96.18 | 92.88 | 83.13 | 68.40 | [Gdrive](https://drive.google.com/file/d/1Uznh1O0EJoD34A3YchvI7FQ1G67rNGKP/view?usp=drive_link) |
+| CosFace-R100 (m=0.35)      | MS1MV2    | 99.75 | 98.41 | 93.25 | 98.37 | 95.93 | 92.33 | 82.85 | 67.38 | [Gdrive](https://drive.google.com/file/d/1FzsD117ESm7RDXE2DMvoQmAdkL7W2sa0/view?usp=drive_link) |
+| CurricularFace-R100 (m=0.5) | MS1MV2    | 99.78 | 98.29 | 92.88 | 98.05 | 96.08 | 91.48 | 82.10 | 66.00 | [Gdrive](https://drive.google.com/file/d/1OPTjbvgBnVBVrttJKvOzevG9sNQXjA1j/view?usp=drive_link) |
+| MagFace-R100               | MS1MV2    | 99.82 | 98.40 | 92.95 | 98.22 | 96.02 | 92.82 | 82.67 | 68.55 | [Gdrive](https://drive.google.com/file/d/1h_V93Sc1NB5eLW26-pB7KCB7-BSCYHZj/view?usp=drive_link) |
+| SphereFace-R100 (m=1.7)    | MS1MV2    | 99.83 | 98.40 | 92.88 | 98.20 | 96.07 | 93.27 | 83.48 | 67.67 | [Gdrive](https://drive.google.com/file/d/1vCMSDF65bslXcU0kxCadBrdW48v-7oPt/view?usp=drive_link) |
+| UniFace-R100 (m=0.5)       | MS1MV2    | 99.78 | 98.49 | 93.28 | 98.02 | 96.10 | 91.63 | 82.18 | 65.75 | [Gdrive](https://drive.google.com/file/d/1TgO7RgXPoMoM6ESIj7h09WYhotou47vD/view?usp=drive_link) |
 
+| Model                         |  Dataset  | LFW | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse | Twins |
+|:------------------------------|:---------:|:------:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:------:|
+| AdaFace-R100 (m=0.4)       | WebFace4M | 99.78 | 99.14 | 94.32 | 97.63 | 96.13 | 91.07 | 82.80 | 72.47 |
+| ArcFace-R100 (m=0.5)       | WebFace4M | 99.77 | 99.21 | 94.25 | 97.85 | 96.12 | 91.78 | 83.20 | 71.73 |
+| ArcFace-R100 (combined)    | WebFace4M | 99.77 | 99.00 | 94.40 | 97.90 | 96.13 | 91.90 | 83.17 | 71.48 |
+| CurricularFace-R100 (m=0.5) | WebFace4M | 99.82 | 98.97 | 94.30 | 97.93 | 96.02 | 90.40 | 82.03 | 70.43 |
+| MagFace-R100               | WebFace4M | 99.80 | 99.23 | 94.25 | 97.88 | 95.97 | 90.92 | 82.47 | 71.08 |
+| UniFace-R100 (m=0.5)       | WebFace4M | 99.77 | 99.17 | 94.47 | 97.60 | 96.02 | 90.65 | 82.20 | 71.82 |
 
-|          WebFace4M          |  Dataset  |  LFW  | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse |                                             Weights                                             |
-|-----------------------------|:---------:|:-----:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:-----------------------------------------------------------------------------------------------:|
-| ArcFace-R100                | WebFace4M | 99.77 | 99.21  | 94.27 |  97.85   | 96.12 |  91.62  |  83.20  | [Gdrive](https://drive.google.com/file/d/1yzm9-VFyVqm9HkQRXDwlLutF0G4tq86x/view?usp=drive_link) |
-| ArcFace-R100 (combined)     | WebFace4M | 99.77 | 99.00  | 94.42 |  97.90   | 96.13 |    -    |    -    | [Gdrive](https://drive.google.com/file/d/15_i01irmE-ruB00qgiU6nOpyPFwKulNz/view?usp=drive_link) |
-| CurricularFace-R100 (m=0.5) | WebFace4M | 99.82 | 99.04  | 94.30 |  97.93   | 95.98 |  90.20  |  81.73  | [Gdrive](https://drive.google.com/file/d/1WUOpOReeaUBmmqfQGXAvl3WMUMfoY4MZ/view?usp=drive_link) |
-| MagFace-R100                | WebFace4M | 99.80 | 99.23  | 94.18 |  97.88   | 95.97 |  90.80  |  82.27  | [Gdrive](https://drive.google.com/file/d/1rVBzy01b_ZWTUJ97ainjS9tWEc2u26Or/view?usp=drive_link) |
-| AdaFace-R100 (m=0.4)        | WebFace4M | 99.78 | 99.14  | 94.32 |  97.63   | 96.13 |  90.50  |  82.28  | [Gdrive](https://drive.google.com/file/d/19uHspLbfkMv0_HfYC3Ege4HQCIsxo8Vr/view?usp=drive_link) |
-| UniFace-R100 (m=0.5)        | WebFace4M | 99.77 | 99.21  | 94.47 |  97.60   | 96.02 |  90.47  |  81.52  | [Gdrive](https://drive.google.com/file/d/1cfJREzrOEqVUAzD5ptH_htIEZiYWdr9H/view?usp=drive_link) |
-
-|          Glint360k          |  Dataset  |  LFW  | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse |                                             Weights                                             |
-|-----------------------------|:---------:|:-----:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:-----------------------------------------------------------------------------------------------:|
-| ArcFace-R100 (m=0.5)        | Glint360k | 99.82 | 99.07  | 94.68 |  98.30   | 96.15 |  95.65  |  83.95  | [Gdrive](https://drive.google.com/file/d/1JsRePpJtVjzgv0N-JSUpyt7ikzsNEaS7/view?usp=drive_link) |
-| CurricularFace-R100 (m=0.5) | Glint360k | 99.78 | 99.11  | 94.68 |  98.43   | 96.17 |  94.05  |  83.38  | [Gdrive](https://drive.google.com/file/d/12pNosc10tOGl-OCH65OH3_rhQ6Le1tMS/view?usp=drive_link) |
-| MagFace-R100                | Glint360k | 99.82 | 99.14  | 94.47 |  98.20   | 96.15 |  94.97  |  83.60  | [Gdrive](https://drive.google.com/file/d/1xg7CBPhatTE1BwmozGIOhI1bfGwWiyUP/view?usp=drive_link) |
-| AdaFace-R100 (m=0.4)        | Glint360k | 99.78 | 99.22  | 95.05 |  98.35   | 96.07 |  95.27  |  83.23  | [Gdrive](https://drive.google.com/file/d/1YRqrXGOao5F3mVQXZ90dt-WLEICNqbYY/view?usp=drive_link) |
-| UniFace-R100 (m=0.5)        | Glint360k | 99.73 | 99.09  | 94.58 |  98.22   | 96.18 |  93.53  |  82.53  | [Gdrive](https://drive.google.com/file/d/1r_LA7F0rmga_ip1MDtoue5lZYBFbtg1-/view?usp=drive_link) |
+| Model                         |  Dataset  | LFW | CFP-FP | CPLFW | AgeDB-30 | CALFW | Hadrian | Eclipse | Twins |
+|:------------------------------|:---------:|:------:|:------:|:-----:|:--------:|:-----:|:-------:|:-------:|:------:|
+| AdaFace-R100 (m=0.4)       | Glint360k | 99.78 | 99.21 | 94.98 | 98.32 | 96.07 | 95.63 | 83.88 | 75.88 |
+| ArcFace-R100 (m=0.5)       | Glint360k | 99.82 | 99.07 | 94.68 | 98.30 | 96.15 | 95.68 | 84.12 | 84.67 |
+| CurricularFace-R100 (m=0.5) | Glint360k | 99.78 | 99.11 | 94.80 | 98.40 | 96.17 | 94.63 | 83.53 | 74.23 |
+| MagFace-R100               | Glint360k | 99.82 | 99.14 | 94.47 | 98.20 | 96.15 | 95.22 | 83.83 | 71.98 |
+| UniFace-R100 (m=0.5)       | Glint360k | 99.73 | 99.09 | 94.58 | 98.22 | 96.18 | 93.53 | 83.18 | 71.52 |
 
 
 # References
--- test.py
+++ test.py
@@ -1,21 +1,46 @@
 import argparse
+import os
 import numpy as np
 import torch
-from torch.nn import DataParallel
-from model import iresnet, PartialFC_V2, get_vit
+import torch.nn as nn
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from model import iresnet, get_vit
 from data.load_test_sets_recognition import get_val_pair
 import verification
+import random
 
+def set_seed(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def setup_ddp():
+    dist.init_process_group(backend="nccl")
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(local_rank)
+    return local_rank
+
+def cleanup_ddp():
+    dist.destroy_process_group()
 
 class Test:
-    def __init__(self, args):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = self.create_model(args)
+    def __init__(self, args, local_rank):
+        self.device = torch.device(f"cuda:{local_rank}")
+        self.local_rank = local_rank
+        self.args = args
+        
+        model = self.create_model(args)
+        model = model.to(self.device)
+        model.eval()
+        
+        self.model = DDP(model, device_ids=[local_rank])
+            
+        self.add_flip = args.add_flip
+        self.add_norm = args.add_norm
+        self.cosine = args.cosine
 
-        if torch.cuda.device_count() > 0:
-            print(f"Model will use {torch.cuda.device_count()} GPUs!")
-            self.model = DataParallel(self.model)
-            self.model = self.model.to(self.device)
         self.validation_list = []
         for val_name in args.val_list:
             dataset, issame = get_val_pair(args.val_source, val_name)
@@ -23,93 +48,110 @@ class Test:
 
     def create_model(self, args):
         if args.model == "iresnet":
-            model = iresnet(args.depth, fp16=True)
+            model = iresnet(args.depth, fp16=True, mode=args.mode)
         elif args.model == "vit":
             model = get_vit(args.depth)
-        model.load_state_dict(torch.load(args.model_path))
+        
+        state_dict = torch.load(args.model_path, map_location='cpu')
+        new_state_dict = {}
+        for k, v in state_dict.items():
+            name = k[7:] if k.startswith('module.') else k
+            new_state_dict[name] = v
+        
+        model.load_state_dict(new_state_dict)
         return model
-
-    def evaluate(self):
-        self.model.eval()
-        val_acc = 0
-        print("Validating...")
-        for idx, validation in enumerate(self.validation_list):
-            dataset, issame, val_name = validation
-            acc, std = self.evaluate_recognition(dataset, issame)
-            print(f"{val_name}: {acc:.5f}+-{std:.5f}")
-            val_acc += acc
-        val_acc /= idx + 1
-
-        print(f"Mean accuracy: {val_acc:.5f}\n")
 
     def l2_norm(self, input: torch.Tensor, axis=1):
         norm = torch.norm(input, 2, axis, True)
         output = torch.div(input, norm)
         return output, norm
 
+    def evaluate(self):
+        if self.local_rank == 0:
+            print("Validating (DDP Mode)...")
+        
+        val_acc = 0
+        for idx, validation in enumerate(self.validation_list):
+            dataset, issame, val_name = validation
+            acc, std = self.evaluate_recognition(dataset, issame)
+            
+            if self.local_rank == 0:
+                print(f"{val_name}: {acc:.5f}+-{std:.5f}")
+                val_acc += acc
+        
+        if self.local_rank == 0:
+            mean_acc = val_acc / len(self.validation_list)
+            print(f"Mean accuracy: {mean_acc:.5f}\n")
+
     def evaluate_recognition(self, samples, issame, nrof_folds=10):
         embedding_length = len(samples) // 2
-        embeddings = np.zeros([embedding_length, 512])
+        world_size = dist.get_world_size()
+        
+        indices = list(range(embedding_length))
+        local_indices = indices[self.local_rank::world_size]
+        
+        local_embeddings = []
 
         with torch.no_grad():
-            for idx in range(0, embedding_length, args.batch_size):
-                batch_flip = torch.tensor(samples[embedding_length + idx: embedding_length + idx + args.batch_size])
-                batch_or = torch.tensor(samples[idx: idx + batch_flip.shape[0]])
-                if args.add_flip:
-                    embeddings[idx: idx + args.batch_size] = self.model(batch_or.to(self.device)).cpu() + \
-                                                             self.model(batch_flip.to(self.device)).cpu()
-                elif args.add_norm:
-                    embeddings_flip, norms_flip = self.l2_norm(self.model(batch_flip.to(self.device)), axis=1)
-                    embeddings_or, norms_or = self.l2_norm(self.model(batch_or.to(self.device)), axis=1)
-                    embeddings[idx: idx + args.batch_size] = (embeddings_flip * norms_flip +
-                                                              embeddings_or * norms_or).cpu()
-                else:
-                    embeddings[idx: idx + args.batch_size] = self.model(batch_or.to(self.device)).cpu()
-                idx += args.batch_size
-        normalized_embedding = np.divide(embeddings, np.linalg.norm(embeddings, 2, 1, True))
+            for i in range(0, len(local_indices), self.args.batch_size):
+                batch_idx = local_indices[i : i + self.args.batch_size]
+                
+                batch_or = torch.tensor(samples[batch_idx]).to(self.device)
+                flip_idx = [idx + embedding_length for idx in batch_idx]
+                batch_flip = torch.tensor(samples[flip_idx]).to(self.device)
+                with torch.cuda.amp.autocast():
+                    if self.add_flip:
+                        feat = (self.model(batch_or) + self.model(batch_flip))
+                    elif self.add_norm:
+                        embeddings_flip, norms_flip = self.l2_norm(self.model(batch_flip), axis=1)
+                        embeddings_or, norms_or = self.l2_norm(self.model(batch_or), axis=1)
+                        feat = (embeddings_flip * norms_flip + embeddings_or * norms_or)
+                    else:
+                        feat = self.model(batch_or)
+                
+                local_embeddings.append(feat.cpu().numpy())
 
-        tpr, fpr, accuracy = verification.evaluate(
-            normalized_embedding, issame, nrof_folds, cosine=args.cosine
-        )
+        if len(local_embeddings) > 0:
+            local_embeddings = np.concatenate(local_embeddings, axis=0)
+        else:
+            local_embeddings = np.empty((0, 512))
 
-        return round(accuracy.mean(), 5), round(accuracy.std(), 5)
+        all_embeddings_list = [None for _ in range(world_size)]
+        dist.all_gather_object(all_embeddings_list, local_embeddings)
+        
+        if self.local_rank == 0:
+            final_embeddings = np.zeros([embedding_length, 512])
+            for r, emb in enumerate(all_embeddings_list):
+                final_embeddings[r::world_size] = emb
+            
+            normalized_embedding = np.divide(final_embeddings, np.linalg.norm(final_embeddings, 2, 1, True))
+            tpr, fpr, accuracy = verification.evaluate(
+                normalized_embedding, issame, nrof_folds, cosine=self.cosine
+            )
+            return accuracy.mean(), accuracy.std()
+        
+        return 0.0, 0.0
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="test a recognition model."
-    )
-    parser.add_argument(
-        "--model_path", "-model_path", help="model path.", type=str
-    )
-    parser.add_argument(
-        "--model", "-model", help="iresnet/vit.", type=str, default="iresnet"
-    )
-    parser.add_argument(
-        "--mode", "-mode", help="using SE attention [normal/se].", type=str, default="normal"
-    )
-    parser.add_argument(
-        "--depth", "-d",
-        help="layers size: resnet [18, 34, 50, 100, 152, 200] / vit [s, b, l].",
-        default="100",
-        type=str
-    )
+    parser = argparse.ArgumentParser(description="test a recognition model.")
+    parser.add_argument("--model_path", "-model_path", help="model path.", type=str)
+    parser.add_argument("--model", "-model", help="iresnet/vit.", type=str, default="iresnet")
+    parser.add_argument("--mode", "-mode", help="using SE attention [normal/se].", type=str, default="normal")
+    parser.add_argument("--depth", "-d", help="layers size: resnet [18, 34, 50, 100, 152, 200] / vit [s, b, l].", default="100", type=str)
     parser.add_argument("--batch_size", "-b", help="Batch size.", default=512, type=int)
     parser.add_argument("--add_flip", "-aflip", help="Add flipped image features.", action="store_true")
     parser.add_argument("--add_norm", "-anorm", help="Add feature norm.", action="store_true")
     parser.add_argument("--cosine", "-cosine", help="cosine distance.", action="store_true")
-    parser.add_argument(
-        "--val_list",
-        "-v",
-        help="List of images to validate, or datasets to validate (recognition).",
-        nargs="+",
-        default=["lfw", "cfp_fp", "cplfw", "agedb_30", "calfw", "hadrian", "eclipse"],
-    )
-    parser.add_argument(
-        "--val_source", "-vs", help="Path to the val images, or dataset LMDB file.", default="./test_sets"
-    )
+    parser.add_argument("--val_list", "-v", help="List of images to validate.", nargs="+", 
+                        default=["lfw", "cfp_fp", "cplfw", "agedb_30", "calfw", "hadrian_even", "eclipse_even", "twins_very_easy"])
+    parser.add_argument("--val_source", "-vs", help="Path to the val images.", default="./test_sets")
 
     args = parser.parse_args()
+    local_rank = setup_ddp()
+    set_seed(42)
 
-    test = Test(args)
+    test = Test(args, local_rank)
     test.evaluate()
+
+    cleanup_ddp()
